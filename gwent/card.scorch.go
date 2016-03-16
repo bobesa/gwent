3229abfacbe7d31cc98c480e0dcc24d3a9ade7a2
Vlastimil Šenfeld <bobesa@gammosaur.com>
2016-03-16 16:50:03 +0100

    ++docs

--- gwent/card.scorch.go
+++ gwent/card.scorch.go
@@ -1,14 +1,17 @@
 package gwent
 
+// CardScorch destroys cards with most power
 type CardScorch struct {
 	BasicCard
 }
 
+// Play put Scorch card to grave and plays Scorch effect
 func (c *CardScorch) Play(p *Player, target Card) {
 	p.Grave = append(p.Grave, c)
 	p.Scorch()
 }
 
+// Type reports that this is a scorch card
 func (c *CardScorch) Type() CardType {
 	return TypeScorch
 }
